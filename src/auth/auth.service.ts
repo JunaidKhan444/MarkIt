d2c977dd3943ced1ec4de53a70eb508e8b7a73fe
Junaid Khan <mrjunaid444@gmail.com>
2023-07-14 14:30:12 +0530

    Config Module Added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,7 +42,25 @@ export class AuthService {
         }
     }
 
-    signin() {
-        return { msg: "I am Sign In" };
+    async signin(dto: AuthDto) {
+        // find the user
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        })
+        // if user does not exist throw exception 
+        //Gaurd Condition
+        if (!user) throw new ForbiddenException("Credentials Incorrect",);
+
+        // compare password 
+        const pwMatches = await argon.verify(user.hash, dto.password);
+
+        // if password incorrect throw exception
+        if (!pwMatches) throw new ForbiddenException("Credentials Incorrect",);
+
+        // send back the user
+        delete user.hash;
+        return user;
     }
 }
